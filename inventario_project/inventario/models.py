c013bf8b8c297ee7b45ac809342ef806c22a879b
Rainier Peña <rainierereys242@gmial.com>
2024-07-04 16:15:06 -0400

    Culminacion

---
+++ inventario_project/inventario/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    marca = models.CharField(max_length=50)
+    cantidad_min = models.IntegerField()
+    cantidad_max = models.IntegerField()
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    
+
